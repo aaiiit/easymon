280170a74e0ace0a336cc862de4ff80db219c4c3
Tom Mestdagh <tmestdagh@digidentity.com>
2016-06-29 16:54:02 +0200

    Added RailsCheck to check if the application is live.

---
+++ lib/easymon/checks/rails_check.rb
@@ -0,0 +1,19 @@
+module Easymon
+  class RailsCheck
+    def check
+      check_status = rails_version_available?
+      if check_status
+        message = "ENABLED"
+      else
+        message = "DISABLED"
+      end
+      [check_status, message]
+    end
+
+    private
+
+    def rails_version_available?
+      Rails.version != nil
+    end
+  end
+end
